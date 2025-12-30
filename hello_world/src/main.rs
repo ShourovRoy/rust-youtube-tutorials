2a2459c90aff660912190e55410c9678480673ab
Shourov Roy <shourovroy2389@gmail.com>
2025-12-31 05:33:29 +0600

    Rust youtube day-01 hello world

---
+++ hello_world/src/main.rs
@@ -0,0 +1,28 @@
+
+
+
+#[derive(Debug)]
+struct Signaure(i32);
+
+
+// main function for executing all the rust codes
+fn main() {
+    // println!("Hello, world!");
+
+    // println!("Hello Moon!")
+
+    // println!("Hello, {1} - {0}", "Shourov", "Roy")
+
+
+    // println!("Hi, I am {name} {title}", name="Srijita", title="Roy")
+
+
+    // println!("Binary value {:x}", 10);
+
+    let sig: Signaure = Signaure(32);
+
+    println!("{:?}", sig);
+
+
+
+}
